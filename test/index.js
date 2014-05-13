7bf5e04f776ba1585b5d7fe4ffe15f32f5f40b98
Ivan Miric <imiric@gmail.com>
2014-05-13 19:48:42 +0200

    Add tests (fixes #1)
    Decided to use in-memory SQLite for this, but perhaps that could be mocked?

---
+++ test/index.js
@@ -0,0 +1,116 @@
+
+/**
+ * Module dependencies.
+ */
+
+var tiq = require('..')({connection: {filename: ':memory:'}}),
+    knex = require('knex').knex,
+    Promise = require('bluebird'),
+    _ = require('lodash'),
+    should = require('chai').should();
+
+beforeEach(function() {
+  return tiq.createSchema().then(function() {
+    return Promise.join(
+      knex.raw('delete from tags'),
+      knex.raw('delete from tags_associations'),
+      knex.raw("delete from sqlite_sequence where name = 'tags'"),
+      knex.raw("delete from sqlite_sequence where name = 'tags_associations'")
+    );
+  });
+})
+
+describe('TiqDB#associate', function() {
+  it('should associate tags with tokens', function(done) {
+    return tiq.associate(['john'], ['hello', 'yes']).then(function() {
+      return knex('tags').select('text').pluck('text').then(function(tags) {
+        tags.should.deep.equal(['john', 'hello', 'yes']);
+        return knex('tags_associations');
+      }).then(function(assocs) {
+        var ids = [];
+        _.each(assocs, function(a) {
+          ids.push(_.values(a));
+        });
+        ids.should.deep.equal([[2, 1], [3, 1]]);
+        done();
+      });
+    });
+  })
+
+  it('should associate tags with tokens using namespaces', function(done) {
+    return tiq.associate(['john'], ['hello', 'yes'], 'private').then(function() {
+      return knex('tags').select('text', 'namespace').then(function(tags) {
+        var cleanTags = [];
+        _.each(tags, function(t) {
+          cleanTags.push(_.values(t));
+        });
+        cleanTags.should.deep.equal([
+          ['john', 'private'], ['hello', 'private'], ['yes', 'private']
+        ]);
+        return knex('tags_associations');
+      }).then(function(assocs) {
+        var ids = [];
+        _.each(assocs, function(a) {
+          ids.push(_.values(a));
+        });
+        ids.should.deep.equal([[2, 1], [3, 1]]);
+        done();
+      });
+    });
+  })
+
+  it('should associate only unique values', function(done) {
+    return Promise.join(
+      tiq.associate(['john'], ['hello', 'yes']),
+      tiq.associate(['another'], ['john', 'peter']),
+      knex('tags').select('text').pluck('text').then(function(tags) {
+        tags.should.deep.equal(['john', 'hello', 'yes', 'another', 'peter']);
+        return knex('tags_associations');
+      }).then(function(assocs) {
+        var ids = [];
+        _.each(assocs, function(a) {
+          ids.push(_.values(a));
+        });
+        ids.should.deep.equal([[2, 1], [3, 1], [1, 4], [5, 4]]);
+        done();
+      })
+    );
+  })
+});
+
+describe('TiqJSON#describe', function() {
+  beforeEach(function() {
+    return Promise.join(
+      knex('tags').insert([
+        {text: 'peter', namespace: 'public'},
+        {text: 'what',  namespace: 'public'},
+        {text: 'peter', namespace: 'private'},
+        {text: 'nope',  namespace: 'private'}
+      ]),
+      knex('tags_associations').insert([
+        {tag_id1: 1, tag_id2: 2},
+        {tag_id1: 3, tag_id2: 4}
+      ])
+    );
+  })
+
+  it('should return the tags associated with the tokens', function(done) {
+    return tiq.describe(['what']).then(function(tags) {
+      tags.should.deep.equal(['peter']);
+      return tiq.describe(['peter']);
+    }).then(function(tags) {
+      tags.should.deep.equal(['what']);
+      done();
+    })
+  })
+
+  it('should return the tags associated with the tokens using namespaces', function(done) {
+    return tiq.describe(['nope'], 'private').then(function(tags) {
+      tags.should.deep.equal(['peter']);
+      return tiq.describe(['peter'], 'private');
+    }).then(function(tags) {
+      tags.should.deep.equal(['nope']);
+      done();
+    })
+  })
+});
